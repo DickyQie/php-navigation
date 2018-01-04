2bc3edb076f3cdf311ebfead53657b8c0d50e249
DickyQie <18523978916@163.com>
2018-01-04 18:12:57 +0800

    分类模块，数据管理，修改密码等

---
+++ Application/Admin/Controller/ElementaryLevelController.class.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Admin\Controller;
+
+use Think\Controller;
+use Think\Page;
+class ElementaryLevelController extends Controller{
+	
+	function lists(){
+		$keyword=I('keyword','','trim');
+		if ($keyword) {
+			$map['elementary_name']=array('like',"%$keyword%");
+			$this->assign('keyword',$keyword);
+		}
+		$element=M('elementary_level');
+		
+		$count=$element->where($map)->count();
+		$row=15;
+		$page=new Page($count,$row);
+		$info=$element->where($map)->order('sort desc')->limit($page->firstRow.','.$page->listRows)->select();
+		
+		$this->assign('datalists',$info);
+		$this->assign('page',$page->show());
+		$this->assign('title','初级分类管理');
+		$this->display();
+	}
+	
+	/***
+	 *添加
+	 */
+	function add() {
+		
+		if (!IS_POST){
+			$high_level=M('high_level');
+			$info=$high_level->select();
+			$this->assign('high_level',$info);
+			$this->display();
+		}else {
+			$data=I('post.');
+			$res=M('elementary_level')->add($data);
+			if ($res){
+				$this->success('添加成功',U('lists'));
+			}else {
+				$this->error("添加失败");
+			}
+		}
+	}
+	/**
+	 * 修改
+	 */
+	
+	function edit() {
+		if (!IS_POST){
+			$id=I('id',0,'int');
+			$element=M('elementary_level');
+			$info=$element->find($id);
+			
+			$high_level=M('high_level')->select();
+			
+			
+			$high_id = M('middle_level')->where(array('id'=>$info['middle_id']))->getField('high_id');
+			
+			
+			$middle=M('middle_level')->where('high_id='.$high_id)->select();
+			
+			$this->assign('data',$info);
+			$this->assign('high_level',$high_level);
+			$this->assign('high_id',$high_id);
+			$this->assign('middle_level',$middle);
+			$this->display();
+		}else {
+			$data=I('post.');
+			$model=M("elementary_level")->where('id='.$data['id'])->save($data);
+			if ($model){
+				$this->success('修改成功',U('lists'));
+			}else{
+				$this->error('修改失败');
+			}
+		}
+	}
+	
+	
+	/***
+	 * 删除
+	 */
+	function delete(){
+		$id=I('id',0,'int');
+		$data=M('datalist')->where('elementary_id='.$id)->select();
+		if($data){
+			$message="该分类下包含其他数据,不能删除";
+			$this->ajaxReturn($message);
+		}else{
+			$res=M('elementary_level')->where('id='.$id)->delete();
+			if ($res){
+				$this->ajaxReturn("删除成功");
+			}else {
+				$this->ajaxReturn("删除失败");
+			}
+		}
+	}
+	
+	/**
+	 *多项删除
+	 */
+	function delAll() {
+		$ids=I('ids',0);
+		$map['elementary_id']=array('in',$ids);
+		$datalist=M('datalist')->where($map)->select();
+		if ($datalist){
+			$message="该分类下包含其他数据,不能删除";
+			$this->ajaxReturn($message);
+		}else {
+			$elid['id']=array('in',$ids);
+			$res=M('elementary_level')->where($elid)->delete();
+			if($res){
+				$mes="删除成功";
+			}else {
+				$mes="删除失败";
+			}
+			$this->ajaxReturn($mes);
+		}
+	}
+	
+	
+	
+	/****
+	 * 联动
+	 * 联表查询
+	 */
+	function get_middle_info() {
+		$map['high_id']=I('high_id',0);
+		$info=M('middle_level')->where($map)->field('id,middle_name')->select();
+		$option='';
+		foreach ($info as $v){
+			$option.="<option value=".$v['id'].">".$v['middle_name']."</option>";
+		}
+		echo $option;
+	}
+	
+	
+}
---
+++ Application/Admin/Controller/MiddleLevelController.class.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Admin\Controller;
+
+use Think\Controller;
+class MiddleLevelController extends Controller{
+	
+	
+	function lists(){
+		
+		$keyword=I('keyword','','trim');
+		if ($keyword){
+			$arr['middle_name']=array('like',"%$keyword%");
+			$this->assign('keyword',$keyword);
+		}
+		
+		$middle=M('middle_level');
+		
+		$count=$middle->where($arr)->count();
+		
+		$row=8;
+		
+		$page=new \Think\Page($count,$row);
+		
+		$info=$middle->where($arr)->order("high_id desc,sort desc")->
+		limit($page->firstRow.','.$page->listRows)->select();
+		
+		
+		
+		//showdata($count);
+		
+		$this->assign('datalists',$info);
+		$this->assign('title','中级分类管理');
+		$this->assign('page',$page->show());
+		
+		
+		$this->display();
+	}
+	
+	/***
+	 * 添加
+	 */
+	function add(){
+		if (IS_POST){
+			$model=M('middle_level');
+			$arr=I('post.');
+			$res=$model->add($arr);
+			if ($res){
+				$message="添加成功";
+				$this->success($message,U('lists'));
+			}else{
+				$message="添加失败";
+				$this->error($message);
+			}
+		}else {
+			$high=M('high_level');
+			$info=$high->select();
+			$this->assign('high_level',$info);
+			$this->assign('type',"add");
+			$this->display();
+		}
+	}
+	
+	
+	/****
+	 * 修改
+	 * 第一：查询  显示修改数据
+	 * 第二：修改数据
+	 */
+	function update(){
+		$middle=M('middle_level');
+		$map['id']=$id=I('id',0,'int');
+		if (IS_POST){
+			$post=I('post.');
+			$res=$middle->where($map)->save($post);
+			if ($res){
+				$this->success('修改成功','lists');
+			}else {
+				$this->error('修改失败');
+			}
+		}else {
+			$high_level=M('high_level');
+			$info=$high_level->select();
+			$data=$middle->where($map)->find();
+			$this->assign('high_level',$info);
+			$this->assign('data',$data);
+			$this->assign('type',"update");
+			$this->display('add');
+		}
+	}
+	
+	
+	/****
+	 * 单个删除
+	 */
+	function delete(){
+		$map['middle_id']=$middle_id=I('id',0,'int');
+		$res=M('elementary_level')->where($map)->select();
+		if ($res){
+			$message="所属分类下有很多子类,不能删除";
+			$this->ajaxReturn($message);
+		}else {
+			$delete=M('middle_level');
+			$res=$delete->where('id='.$middle_id)->delete();
+			if ($res){
+				$message="删除成功";
+			}else {
+				$message="删除失败";
+			}
+			$this->ajaxReturn($message);
+		}
+		
+	}
+	
+	/****
+	 * 多个删除
+	 */
+	function delAll(){
+		$ids=I('ids',0);
+		$where['middle_id']=array('in',$ids);
+		$res=M('elementary_level')->where($where)->select();
+	 	if ($res){
+			$message="该分类下包含多个文件,不能删除";
+			$this->ajaxReturn($message);
+		}else {
+			$id['id']=array('in',$ids);
+			$rmn=M('middle_level')->where($id)->delete();
+			if ($rmn){
+				$this->ajaxReturn("删除成功");
+			}else {
+				$this->ajaxReturn("删除失败");
+			}
+		} 
+	}
+	
+}
